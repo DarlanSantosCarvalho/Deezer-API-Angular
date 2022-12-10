6c03de54d0d86e088fdd3239e966315a8f7cc4bd
Darlan Carvalho <darlan171201@gmail.com>
2022-12-10 15:59:53 -0300

    Comecei o projeto, estou apenas alguns passos de finalizar:

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'deezer-angular';
+
+  imagePath: string;
+
+  constructor(){
+    //image location
+    this.imagePath = '/assets/deezer-logo-2021.svg'
+  }
+}
