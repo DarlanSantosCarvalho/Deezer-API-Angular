539fa0f3e637fbe045ad15efde6978a94e231fa5
Darlan Carvalho <darlan171201@gmail.com>
2022-12-13 23:34:02 -0300

    Projeto quase completo, falta adicionar o value ao params

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { HttpClient } from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
-  title = 'deezer-angular';
+  conteudoSalvo: string = '';
+  tracks: any = [];
+  cover: any = [];
+  constructor(private http: HttpClient){
 
-  imagePath: string;
-
-  imagePathSearchButton: string;
-
-  constructor(){
-    //image location
-    this.imagePath = '/assets/deezer-logo-2021.svg'
-
-    this.imagePathSearchButton = '/assets/magnifier.png'
   }
 
-}
+  onSave(valor:string){
+    this.conteudoSalvo = valor;
+    console.log(valor)
+  }
+
+  ngOnInit(): void {
+      this.getMethodPlaylists();
+      this.getMethodSearch();
+
+  }
+
+  public getMethodPlaylists(){
+    const options = {
+      method: 'GET',
+      headers: {
+        'X-RapidAPI-Key': '00409c6540msh20ba7d743bf8d70p10020djsnb1ca8717e8c6',
+        'X-RapidAPI-Host': 'deezerdevs-deezer.p.rapidapi.com'
+      }
+    };
+    this.http.get('https://deezerdevs-deezer.p.rapidapi.com/playlist/3155776842?limit=5', options).subscribe((data:any) =>{
+      this.tracks = data.tracks.data;
+    });
+  }
+
+  public async getMethodSearch(){
+    const options = {
+      method: 'GET',
+      url: 'https://deezerdevs-deezer.p.rapidapi.com/search',
+      params: {q: 'Eminem' ,"limit": 1}, // Usar event binding no parametro da função e usar o mesmo parametro na querie
+      headers: {
+        'X-RapidAPI-Key': '00409c6540msh20ba7d743bf8d70p10020djsnb1ca8717e8c6',
+        'X-RapidAPI-Host': 'deezerdevs-deezer.p.rapidapi.com'
+      }
+    };
+    this.http.get(`https://deezerdevs-deezer.p.rapidapi.com/search`, options).subscribe((data:any) =>{
+      console.log(data)
+  })
+  }
+
+  }
 
 
